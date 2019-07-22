faea26fb632751aa35c8323ea05615ecc1a54b22
brookiestein <brookie1@protonmail.com>
2019-07-21 22:07:23 -0400

    Adding the license

--- bash/kernel.sh
+++ bash/kernel.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+# Copyright (C) 19aa Lord Brookie
+# Este programa es software libre. Puede redistribuirlo y/o
+# modificarlo bajo los términos de la Licencia Pública General
+# de GNU según es publicada por la Free Software Foundation,
+# bien de la versión 2 de dicha Licencia o bien --según su
+# elección-- de cualquier versión posterior.
+# Este programa se distribuye con la esperanza de que sea
+# útil, pero SIN NINGUNA GARANTÍA, incluso sin la garantía
+# MERCANTIL implícita o sin garantizar la CONVENIENCIA PARA UN
+# PROPÓSITO PARTICULAR. Para más detalles, véase la Licencia
+# Pública General de GNU.
+# Debería haber recibido una copia de la Licencia Pública
+# General junto con este programa. En caso contrario, escriba
+# a la Free Software Foundation, Inc., en 675 Mass Ave,
+# Cambridge, MA 02139, EEUU.
+
 message="¿Desea tener soporte para"
 answer="La respuesta introducida no es válida."
 get_threads() {
