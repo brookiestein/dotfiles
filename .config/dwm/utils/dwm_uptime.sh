20616195fdd5380521116beb5c75800542a84b01
LordBrookie <brookie1@protonmail.com>
2020-01-11 08:49:44 -0400

    Solution that only shows the on time after one hour.

--- .config/dwm/utils/dwm_uptime.sh
+++ .config/dwm/utils/dwm_uptime.sh
@@ -5,12 +5,28 @@
 
 dwm_uptime() {
         printf "$SEP1"
-        up="$(uptime | cut -d ' ' -f 5 | cut -d ',' -f 1)"
-        hour=$(echo $up | cut -d ':' -f 1)
-        if [ $hour -lt 10 ]; then
-                up="Up:0$hour H|$(echo $up | cut -d ':' -f 2) M"
+        up="$(uptime | cut -d ' ' -f 4 | cut -d ',' -f 1)"
+
+        # Verificar si es un número entero:
+        echo -en $up | grep '[[:digit:]]' > /dev/null 2>&1
+        if [ $? -ne 0 ]; then
+                up="$(uptime | cut -d ' ' -f 5 | cut -d ',' -f 1)"
+                isdigit="False"
         else
-                up="Up:$hour H|$(echo $up | cut -d ':' -f 2) M"
+                isdigit="True"
+        fi
+
+        # Si es un número entero, se asume que tiene menos de
+        # una hora encendido el equipo.
+        if [ "$isdigit" = "True" ]; then
+                up="Up:0 H|$up M"
+        else
+                hour=$(echo $up | cut -d ':' -f 1)
+                if [ $hour -lt 10 ]; then
+                        up="Up:0$hour H|$(echo $up | cut -d ':' -f 2) M"
+                else
+                        up="Up:$hour H|$(echo $up | cut -d ':' -f 2) M"
+                fi
         fi
         printf "$up"
         printf "$SEP2"
