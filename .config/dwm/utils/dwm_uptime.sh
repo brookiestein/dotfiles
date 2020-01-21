c401ac34708960adc6a88f09634e92ee50313b44
LordBrookie <brookie1@protonmail.com>
2020-01-21 10:02:18 -0400

    Some improvements.

--- .config/dwm/utils/dwm_uptime.sh
+++ .config/dwm/utils/dwm_uptime.sh
@@ -6,26 +6,35 @@
 dwm_uptime() {
         printf "$SEP1"
         up="$(uptime | cut -d ' ' -f 5 | cut -d ',' -f 1)"
-
-        # Verificar si es un número entero:
-        echo -en $up | grep '[[:digit:]]' > /dev/null 2>&1
-        if [ $? -ne 0 ]; then
-                up="$(uptime | cut -d ' ' -f 4 | cut -d ',' -f 1)"
-                isdigit="False"
-        else
-                isdigit="True"
-        fi
-
-        # Si no es un número entero, se asume que tiene menos de
-        # una hora encendido el equipo.
-        if [ "$isdigit" = "False" ]; then
-                up="Up:0 H|$up M"
-        else
+        if [ $up = "day" ]; then
+                day="$(uptime | cut -d ' ' -f 4 | cut -d ',' -f 1)"
+                up="$(uptime | cut -d ' ' -f 6 | cut -d ',' -f 1)"
                 hour=$(echo $up | cut -d ':' -f 1)
-                if [ $hour -lt 10 ]; then
-                        up="Up:0$hour H|$(echo $up | cut -d ':' -f 2) M"
+
+                echo -en $hour | grep '[[:digit:]]' > /dev/null 2>&1
+                if [ $? -ne 0 ]; then
+                        hour=0
+                fi
+
+                min=$(echo $up | cut -d ':' -f 2)
+                up="Up:$day D|$hour H|$min M"
+        else
+                echo -en $up | grep '[[:digit:]]' > /dev/null 2>&1
+                if [ $? -ne 0 ]; then
+                        up="$(uptime | cut -d ' ' -f 4 | cut -d ',' -f 1)"
+                        isdigit="False"
                 else
-                        up="Up:$hour H|$(echo $up | cut -d ':' -f 2) M"
+                        isdigit="True"
+                fi
+
+                # Si no es un número entero, se asume que tiene menos de
+                # una hora encendido el equipo.
+                if [ "$isdigit" = "False" ]; then
+                        up="Up:0 D|0 H|$up M"
+                else
+                        hour=$(echo $up | cut -d ':' -f 1)
+                        min=$(echo $up | cut -d ':' -f 2)
+                        up="Up:0 D|$hour H|$min M"
                 fi
         fi
         printf "$up"
