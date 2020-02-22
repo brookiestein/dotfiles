a52965af5b5c493763f35d7498829c0987aa49cd
Lord Brookie <45701374+brookiestein@users.noreply.github.com>
2020-02-22 19:07:35 -0400

    Little error in the command.

--- .scripts/resolution.sh
+++ .scripts/resolution.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-status="$(xrandr | grep VGA | cud -d ' ' -f 2)"
+status="$(xrandr | grep VGA | cut -d ' ' -f 2)"
 if [ "$status" = "connected" ]; then
         xrandr --output LVDS-1 --auto --primary
         xrandr --output VGA-1 --auto --right-of LVDS-1 --mode 1280x1024
