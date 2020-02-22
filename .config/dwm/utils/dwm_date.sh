4b15274d42ddb360d737a3c2aa42d92de0bbec11
brookiestein <brookie1@protonmail.com>
2020-02-21 22:35:23 -0400

    Script for show the time now shown the day of the week too.

--- .config/dwm/utils/dwm_date.sh
+++ .config/dwm/utils/dwm_date.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-# Date is formatted like this: "[YYYY-mm-dd 00:00:00]"
+# Date is formatted like this: "[day YYYY-mm-dd 00:00:00]"
 function dwm_date
 {
+        day=$(date +%a)
         local_date=$(date +%Y-%m-%d)
         time=$(date +%T)
-        printf "%s%s %s%s" "$SEP1" "$local_date" "$time" "$SEP2"
+        printf "%s%s %s%s%s" "$SEP1" "$day" "$local_date" "$time" "$SEP2"
 }
 
 dwm_date
