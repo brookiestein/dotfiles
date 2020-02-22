9c1ca928105fb4c0baa4553e27e66f441cc50fdc
brookiestein <brookie1@protonmail.com>
2020-02-22 02:22:04 -0400

    Little script for automatically configure a second monitor with a resolution different to the first monitor.

---
+++ .scripts/resolution.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+status="$(xrandr | grep VGA | cud -d ' ' -f 2)"
+if [ "$status" = "connected" ]; then
+        xrandr --output LVDS-1 --auto --primary
+        xrandr --output VGA-1 --auto --right-of LVDS-1 --mode 1280x1024
+fi
