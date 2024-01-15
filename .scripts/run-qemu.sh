e9b80ea6690efb1bed08d89c3f898cb1806b7d09
brookiestein <this.brayan@proton.me>
2024-01-15 09:40:50 -0400

    Added some new options and functionality:
    * Default CPU cores will default to all of your CPU cores.
      You can use the placeholder 'half' if you just want to use half of
      them.
    * Default amount of RAM will default to half of your system's RAM.
    * Added pretend option just to show the options this script will run
      with.

--- .scripts/run-qemu.sh
+++ .scripts/run-qemu.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+SMP=`nproc`
 CDROM=
 HDA=
 NAME="VM"
-AMOUNT_OF_RAM="4G"
+AMOUNT_OF_RAM=`free -h | awk '{print $2;}' | head -n 2 | tail -n 1 | cut -d 'G' -f 1 | cut -d '.' -f 1`
+AMOUNT_OF_RAM="$((AMOUNT_OF_RAM/2))G"
+PRETEND=0
 VERBOSE=0
 
 usage() {
@@ -12,15 +15,21 @@ usage() {
     echo -e "-c\t<cdrom>"
     echo -e "-H\tThis help."
     echo -e "-h\t<hard disk drive file>."
-    echo -e "-n\t<VM name>."
-    echo -e "-r\t<Amount of RAM> (Default is 4G)."
+    echo -e "-n\t<VM name>. (1)"
+    echo -e "-p\tPretend. In other words, just show the options it'll be run with."
+    echo -e "-r\t<Amount of RAM> (2)"
+    echo -e "-s\t<Amount of cores> (3)"
     echo -e "-v\tBe verbose."
     echo
-    echo "Try not to use whitespaces in the VM name. If you do, qemu will complain."
+    echo "1) Try not to use whitespaces in the VM name. If you do, qemu will complain."
+    echo "2) If you don't specify the amount of RAM, this script will default to half your system's RAM."
+    echo "3) If you don't specify the amount of cores, this script will default to all of your processor cores."
+    echo "   You can also use the placeholder 'half' to just use half your processor's cores."
+    echo
     echo "Notice that you can combine all of the options, except -H."
 }
 
-while getopts "c:Hh:n:r:v" arg; do
+while getopts "c:Hh:n:pr:s:v" arg; do
     case ${arg} in
         c)
             CDROM=$OPTARG
@@ -35,8 +44,19 @@ while getopts "c:Hh:n:r:v" arg; do
         n)
             NAME=$OPTARG
             ;;
+        p)
+            PRETEND=1
+            ;;
         r)
-            AMOUNT_OF_RAM=$OPTARG
+            AMOUNT_OF_RAM="${OPTARG}"
+            ;;
+        s)
+            if [ $OPTARG == "half" ]; then
+                SMP=`nproc`
+                SMP=$((SMP/2))
+            else
+                SMP=$OPTARG
+            fi
             ;;
         v)
             VERBOSE=1
@@ -44,7 +64,8 @@ while getopts "c:Hh:n:r:v" arg; do
     esac
 done
 
-ARGS="-cpu host -smp 4 -enable-kvm -m ${AMOUNT_OF_RAM} \
+ARGS="-cpu host -smp ${SMP} \
+-enable-kvm -m ${AMOUNT_OF_RAM} \
 -hda ${HDA} \
 -netdev user,id=vmnic,hostname=${NAME} \
 -device virtio-net,netdev=vmnic \
@@ -56,6 +77,10 @@ if [ $VERBOSE -eq 1 ]; then
     echo "Running with options:"
     echo "${ARGS}"
     echo
+fi
+
+if [ $PRETEND -gt 0 ]; then
+    exit 0
 fi
 
 qemu-system-x86_64 ${ARGS}
