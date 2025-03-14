fc249f0c5800e62c979d1d2bf8d66ff761f3a2f9
Brayan M. Salazar <this.brayan@proton.me>
2025-03-14 11:43:04 -0400

    Updated emacs, sway and waybar config files.

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,6 +1,17 @@
 ;; Disable backup files.
 (setf make-backup-files nil)
 ;; Show line numbers.
-(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+(global-display-line-numbers-mode 1)
 ;; Save cursor position
 (save-place-mode 1)
+;; Set indentation
+(setq-default indent-tabs-mode nil)
+;; (setq-default tab-width 4)
+(setq-default
+ indent-tabs-mode t
+ tab-stop-list (number-sequence 4 200 4)
+ tab-width 4
+ indent-line-function 'insert-tab)
+;; Coding style
+(setq c-default-style "linux")
+(c-set-offset 'comment-intro 0)
