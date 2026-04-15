bee2e81e98a6bfdd94cf82a1d8c46301bb2ff40e
Brayan M. Salazar <this.brayan@proton.me>
2026-04-14 20:55:51 -0400

    Updated ACPI LID handler.

--- etc/acpi/lid_handler.sh
+++ etc/acpi/lid_handler.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-case "$3" in
-    close)
+case "$*" in
+    *close*)
+	su - -c 'DISPLAY=:0 /usr/local/bin/slock' brayan &
 	echo mem > /sys/power/state
 	;;
-    open)
+    *open*)
 	;;
 esac
